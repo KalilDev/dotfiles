b561bd6de0ad4a3fd054f003d9e58e2099f365df
KalilDev <kalilgamer0@gmail.com>
2020-08-31 01:13:56 -0300

    install_home_symlinks: Harden logic
    * Also remove .Xclients

--- install_home_symlinks.sh
+++ install_home_symlinks.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
 
-currentFolder=${PWD##*/}
-userHomeRegex="\/home\/.*?\/$currentFolder"
+repoFolder="$HOME/dotfiles"
 
 # Check if the repository was cloned in /home/user, if not exit
-if [[ ! "$PWD" =~ "$userHomeRegex" ]]; then
-    return 1
+if [[ ! -d "$repoFolder" ]]; then
+    echo "Clone this repo in $repoFolder"
     exit 1
 fi
 
-userHome="$HOME"
 
 dotFiles=$(cat <<EOF
 .profile
-.Xclients
 .xinitrc
 .zsh_favlist
 .zshrc
 EOF
 )
 
+link () {
+    source="$1"
+    destination="$2"
+
+    if [[ -f "$destination" ]] || [[ -d "$destination" ]] && [[ ! -h "$destination" ]]; then
+        echo "$destination exists and is not an link. Check it out"
+        return 1
+    fi
+
+    if [[ -h "$destination" ]]; then
+        actualDestination="$(readlink -f "$destination")"
+        if [[ "$actualDestination" = "$source" ]]; then
+            echo "$source is already linked to $destination"
+            return 0
+        else
+            echo "$destination is already linked to $actualDestination. Override? [Yy/Nn]"
+            read response
+            if [[ "$response" != "N" ]] || [[ "$response" != "n" ]]; then
+                return 1
+            fi
+        fi
+    fi
+
+    echo "Linking \"$source\" to \"$destination\""
+    ln -s "$source" "$destination"
+    return 0
+}
+
 while IFS= read -r dotFile; do
-    echo "Linking \"$PWD/$dotFile\" to \"$userHome/$dotFile\""
-    ln -s "$PWD/$dotFile" "$userHome/$dotFile"
+    link "$repoFolder/$dotFile" "$HOME/$dotFile"
 done < <(printf '%s\n' "$dotFiles")
 
-configFiles="$(ls .config)"
-if [[ "$configFiles" == "" ]]; then
-    return 0
-    exit 0
-fi
+configFiles="$(ls $repoFolder/.config)"
 
 while IFS= read -r configFolder; do
-    echo "Linking \"$PWD/.config/$configFolder\" to \"$userHome/.config/$configFolder\""
-    ln -s "$PWD/.config/$configFolder" "$userHome/.config/$configFolder"
+    link "$repoFolder/.config/$configFolder" "$HOME/.config/$configFolder"
 done < <(printf '%s\n' "$configFiles")
